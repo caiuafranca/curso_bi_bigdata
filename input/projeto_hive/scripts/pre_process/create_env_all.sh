7976cbd0cca32b4000aac9408bc8274d7ddf57e7
Caiuá França <caiua_franca@hotmail.com>
2023-03-30 18:06:16 +0000

    updated at

--- input/projeto_hive/scripts/pre_process/create_env_all.sh
+++ input/projeto_hive/scripts/pre_process/create_env_all.sh
@@ -7,13 +7,13 @@ DADOS=("cidade" "estado" "filial" "parceiro" "cliente" "subcategoria" "categoria
 for i in "${DADOS[@]}"
 do
 	echo "$i"
-    mkdir ../../raw/$i
-    chmod 777 ../../raw/$i
-    cd ../../raw/$i
-    curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$i.csv
+    #mkdir ../../raw/$i
+    #chmod 777 ../../raw/$i
+    #cd ../../raw/$i
+    #curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$i.csv
     hdfs dfs -mkdir /datalake/raw/$i
     hdfs dfs -chmod 777 /datalake/raw/$i
     hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
-    #beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
-    #beeline -u jdbc:hive2://localhost:10000 -e `Select count(*) as quantidade from desafio.{$i};`
+    beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
+    beeline -u jdbc:hive2://localhost:10000 -e `Select count(*) as quantidade from desafio.{$i};`
 done
